1472311b4118c0c72569bcc0bef981db87d4bf08
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-08 12:38:11 +0600

    new

--- main/serializers.py
+++ main/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from .models import Category, Product
 
 
-class CategorySerializers(ModelSerializer):
+class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
@@ -11,3 +11,8 @@ class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializers(instance.category).data
+        return rep
