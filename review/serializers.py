c31da24e3adef0016daec845c97589d5dadd5f25
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-12 12:33:34 +0600

    product admin inlines

--- review/serializers.py
+++ review/serializers.py
@@ -5,9 +5,22 @@ from .models import Comment, Rate
 class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        exclude = ('author',)
+
+    def validate(self, attrs):
+        attrs =  super().validate(attrs)
+        request = self.context.get('request')
+        attrs['author'] = request.user
+        return attrs
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        del rep['product']
+        rep['author'] = instance.author.email
+        return rep
+
 
 class RatingSerializer(ModelSerializer):
     class Meta:
         model = Rate
-        fields = '__all__'
+        exclude = ('author',)
