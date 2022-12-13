8963029fd1b62f27fef3c3555038256bb897b577
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-13 10:53:46 +0600

    create rating view

--- review/serializers.py
+++ review/serializers.py
@@ -24,3 +24,9 @@ class RatingSerializer(ModelSerializer):
     class Meta:
         model = Rate
         exclude = ('author',)
+
+    def validate(self, attrs):
+        attrs =  super().validate(attrs)
+        request = self.context.get('request')
+        attrs['author'] = request.user
+        return attrs
